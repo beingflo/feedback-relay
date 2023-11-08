b1688bfd13ccf8a1eca370a2833b88c1a5306a63
Florian Marending <florian@marending.dev>
2023-11-08 18:00:48 +0100

    Add proof of concept email sending

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,35 @@
-fn main() {
-    println!("Hello, world!");
+use dotenv::dotenv;
+use serde::Serialize;
+
+#[derive(Serialize)]
+struct Request {
+    from: String,
+    to: Vec<String>,
+    subject: String,
+    text: String,
+}
+
+#[tokio::main]
+async fn main() -> Result<(), Box<dyn std::error::Error>> {
+    dotenv().ok();
+    let api_key = std::env::var("RESEND_API_KEY").expect("Resend api key must be set");
+
+    let request = Request {
+        from: "onboarding@resend.dev".to_string(),
+        to: vec!["florian@marending.dev".to_string()],
+        subject: "Test from rust".to_string(),
+        text: "It works!".to_string(),
+    };
+
+    let client = reqwest::Client::new();
+
+    let resp = client
+        .post("https://api.resend.com/emails")
+        .header("Authorization", format!("Bearer {}", api_key))
+        .header("Content-Type", "application/json")
+        .json(&request)
+        .send()
+        .await?;
+    println!("{:#?}", resp);
+    Ok(())
 }
