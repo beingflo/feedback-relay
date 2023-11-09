6a47596b2c5942405c942382cf1431e671f85c15
Florian Marending <florian@marending.dev>
2023-11-09 21:18:42 +0100

    Add rate limiting

--- src/main.rs
+++ src/main.rs
@@ -1,35 +1,99 @@
+use std::time::Duration;
+
+use axum::error_handling::HandleErrorLayer;
+use axum::{http::StatusCode, routing::post, Json, Router};
 use dotenv::dotenv;
-use serde::Serialize;
+use serde::{Deserialize, Serialize};
+use tower::buffer::BufferLayer;
+use tower::limit::RateLimitLayer;
+use tower::{BoxError, ServiceBuilder};
 
 #[derive(Serialize)]
-struct Request {
+struct EmailRequest {
     from: String,
     to: Vec<String>,
     subject: String,
     text: String,
 }
 
+#[derive(Serialize, Deserialize)]
+struct FeedbackRequest {
+    project: String,
+    path: String,
+    email: String,
+    content: String,
+}
+
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
     dotenv().ok();
+    let app = Router::new().route("/feedback", post(handler)).layer(
+        ServiceBuilder::new()
+            .layer(HandleErrorLayer::new(|err: BoxError| async move {
+                (
+                    StatusCode::INTERNAL_SERVER_ERROR,
+                    format!("Unhandled error: {}", err),
+                )
+            }))
+            .layer(BufferLayer::new(1024))
+            .layer(RateLimitLayer::new(4, Duration::from_secs(60))),
+    );
+
+    axum::Server::bind(&"0.0.0.0:8080".parse().unwrap())
+        .serve(app.into_make_service())
+        .await
+        .unwrap();
+
+    Ok(())
+}
+
+async fn handler(Json(body): Json<FeedbackRequest>) -> StatusCode {
+    match send_email(body.project, body.path, body.email, body.content).await {
+        Ok(response) => {
+            println!("Email sent with status: {}", response.status());
+            if response.status() == 200 {
+                StatusCode::OK
+            } else {
+                StatusCode::INTERNAL_SERVER_ERROR
+            }
+        }
+        Err(error) => {
+            println!("Email sent with error: {}", error);
+            StatusCode::INTERNAL_SERVER_ERROR
+        }
+    }
+}
+
+async fn send_email(
+    project: String,
+    path: String,
+    author_email: String,
+    content: String,
+) -> Result<reqwest::Response, reqwest::Error> {
     let api_key = std::env::var("RESEND_API_KEY").expect("Resend api key must be set");
 
-    let request = Request {
+    let request = EmailRequest {
         from: "onboarding@resend.dev".to_string(),
         to: vec!["florian@marending.dev".to_string()],
-        subject: "Test from rust".to_string(),
-        text: "It works!".to_string(),
+        subject: format!("Feedback: {} - {}", project, path),
+        text: format!(
+            "{}\n\n{}",
+            if author_email.is_empty() {
+                "Anonymous"
+            } else {
+                &author_email
+            },
+            content
+        ),
     };
 
     let client = reqwest::Client::new();
 
-    let resp = client
+    client
         .post("https://api.resend.com/emails")
         .header("Authorization", format!("Bearer {}", api_key))
         .header("Content-Type", "application/json")
         .json(&request)
         .send()
-        .await?;
-    println!("{:#?}", resp);
-    Ok(())
+        .await
 }
